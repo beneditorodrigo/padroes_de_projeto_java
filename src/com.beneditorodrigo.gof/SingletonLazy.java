e3bad45e062d36ccced13d454a67951d618571fa
beneditorodrigo <rodrigodacunha7@gmail.com>
2022-10-24 23:53:04 -0300

    Exemplo de padrão Singleton no modo preguiçoso

---
+++ src/com.beneditorodrigo.gof/SingletonLazy.java
@@ -0,0 +1,22 @@
+package com.beneditorodrigo.gof;
+
+/**
+ * Singleton "preguiçoso"
+ *
+ * @author beneditorodrigo
+ */
+public class SingletonLazy {
+
+    private static SingletonLazy instancia;
+
+    private SingletonLazy(){
+        super();
+    }
+
+    public static SingletonLazy getInstance(){
+        if (instancia == null){
+            instancia = new SingletonLazy();
+        }
+        return instancia;
+    }
+}
